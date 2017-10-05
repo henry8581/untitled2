7bea85067b481fd790981146a0ecd7794f5f1759
henry8581 <henry8581@yahoo.com.tw>
2017-10-05 21:27:54 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+package Main;
+
+public class Main {
+
+    public static void main(String[] args) {
+	       java.util.Scanner sc = new java.util.Scanner(System.in);
+	       System.out.println("Please enter the number");
+	       System.out.println("Please enter the number");
+	       System.out.println("Please enter the number");
+	       double n1=sc.nextDouble();
+	       double n2=sc.nextDouble();
+	       double n3=sc.nextDouble();
+
+	       double sum=n1+n2+n3;
+	       double average=sum/3;
+
+	       System.out.println(sum +" "+average);
+    }
+}
